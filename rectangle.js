5ffca315a46ab0a213192d2703b2a96c98241230
Idba1 <moniraislamself@gmail.com>
2024-02-15 14:15:25 +0600

    rectangle

--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,20 @@
 function calculateRectangleArea (){
-    console.log('calculate rect area');
+    // Get length of the rectangle
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthValueInText = lengthInput.value;
+    const lengthValue = parseFloat(lengthValueInText);
+    console.log(lengthValue);
+
+    // Get width of the rectangle
+    const widthInput = document.getElementById('rectangle-width');
+    const widthValue = parseFloat(widthInput.value);
+    console.log(widthValue);
+
+    // Calculate rectangle Area
+    const area = lengthValue * widthValue;
+    console.log('Area of the rectangle : ',area);
+
+    // display rectangle area
+    const areaValue = document.getElementById('rec-area');
+    areaValue.innerText = area;
 }
