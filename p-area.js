e2a61cad4c6602b63824198c64175263b0b11844
Idba1 <moniraislamself@gmail.com>
2024-02-15 15:22:43 +0600

    parallelogram base&height

---
+++ p-area.js
@@ -0,0 +1,13 @@
+// console.log('hello');
+
+function calculateParaArea (){
+    // get paralogram base value 
+    const parallogramBaseInput = document.getElementById('para-base');
+    const parallogramBaseInputValue = parseFloat(parallogramBaseInput.value);
+    console.log(parallogramBaseInputValue);
+
+    // get paralogram height value
+    const parallogramHeightInput = document.getElementById('para-height');
+    const parallogramHeightInputValue = parseFloat(parallogramHeightInput.value);
+    console.log(parallogramHeightInputValue);
+}
